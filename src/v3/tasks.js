dfd49741cdb58310016c177fbfb8c37edf3f50cc
Rjnishant <singhnishant530@gmail.com>
2023-09-23 20:08:19 +0530

    method 3

--- src/v3/tasks.js
+++ src/v3/tasks.js
@@ -1,26 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { apiCallBegin } from "./api";
 
 var id = 0;
 
+const initialState = {
+  loading: false,
+  tasks: [],
+  error: null,
+};
+
 const taskSlice = createSlice({
-  initialState: [],
+  initialState,
   name: "tasks",
   reducers: {
+    apiRequested: (state, action) => {
+      state.loading = true;
+    },
+    apiRequestFailed: (state, action) => {
+      state.loading = false;
+    },
+    gettask: (state, action) => {
+      state.tasks = action.payload;
+      state.loading = false;
+    },
     addTask: (state, action) => {
-      state.push({
+      state.tasks.push({
         id: ++id,
         task: action.payload.task,
         completed: false,
       });
     },
     removeTask: (state, action) => {
-      return state.filter((task) => task.id !== action.payload.id);
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload.id);
     },
     completeTask: (state, action) => {
-      return state.map((v) => (v.id === action.payload.id ? { ...v, completed: true } : v));
+      state.tasks = state.tasks.map((v) => (v.id === action.payload.id ? { ...v, completed: true } : v));
     },
   },
 });
 
-export const {addTask,removeTask,completeTask} = taskSlice.actions
+export const { addTask, removeTask, completeTask, gettask, apiRequested, apiRequestFailed } = taskSlice.actions;
 export default taskSlice.reducer;
+
+//Action creators
+
+export const loadTask = apiCallBegin({
+  url: "/tasks",
+  method: "GET",
+  onStart: apiRequested.type,
+  onSuccess: gettask.type,
+  onError: apiRequestFailed.type,
+});
