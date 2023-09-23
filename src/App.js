d45eb578ccc6ca927e167ac6caf231cd4ebad778
Rjnishant <singhnishant530@gmail.com>
2023-09-23 13:37:03 +0530

    method 2 : createAsyncThunk

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import store from "./v3/store";
-import { addTask, removeTask, completeTask, putTask } from "./v3/tasks";
+import { addTask, removeTask, completeTask, fetchTask } from "./v3/tasks";
 
-const method1 = async () => {
-  try {
-    const data = await fetch("http://localhost:5000/api/tasks");
-    const tasks = await data.json();
-    store.dispatch(putTask({ tasks }));
-  } catch (error) {
-    store.dispatch({ type: "SHOW_ERROR", payload: { error: error.message } });
-  }
-};
 
 function App() {
-  method1();
+  store.dispatch(fetchTask())
   return (
     <div className="App">
       <header className="App-header">
--- src/v3/tasks.js
+++ src/v3/tasks.js
@@ -1,26 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchTask = createAsyncThunk("fetchTask", async (a, rejectWithValue) => {
+  try {
+    const data = await fetch("http://localhost:5000/api/tasks");
+    const tasks = await data.json();
+    return { tasks };
+  } catch (error) {
+    return rejectWithValue({ error: error.message });
+  }
+});
 
 var id = 0;
+const initialState = {
+  loading: false,
+  tasks: [],
+  error: null,
+};
 
 const taskSlice = createSlice({
-  initialState: [],
+  initialState,
   name: "tasks",
   reducers: {
-    putTask: (state, action) => {
-      return action.payload.tasks;
-    },
     addTask: (state, action) => {
-      state.push({
+      state.tasks.push({
         id: ++id,
         task: action.payload.task,
         completed: false,
       });
     },
     removeTask: (state, action) => {
-      return state.filter((task) => task.id !== action.payload.id);
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload.id);
     },
     completeTask: (state, action) => {
-      return state.map((v) => (v.id === action.payload.id ? { ...v, completed: true } : v));
+      state.tasks = state.tasks.map((v) => (v.id === action.payload.id ? { ...v, completed: true } : v));
+    },
+  },
+  extraReducers: {
+    [fetchTask.fulfilled]: (state, action) => {
+      state.tasks.push(...action.payload.tasks);
+      state.loading = false;
+    },
+    [fetchTask.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchTask.error]: (state, action) => {
+      state.error = action.payload.error;
+      state.loading = false;
     },
   },
 });
