d37d04bffe70c162d4df90307d7318050be66de2
Rjnishant <singhnishant530@gmail.com>
2023-09-23 10:31:18 +0530

    use createSlice

--- src/v2/tasks.js
+++ src/v2/tasks.js
@@ -1,24 +1,26 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const addTask = createAction("ADD_TASK");
-export const removeTask = createAction("REMOVE_TASK");
-export const completeTask = createAction("COMPLETE_TASK");
-
-const initialState = [];
 var id = 0;
 
-export default createReducer(initialState, {
-  [addTask.type]: (state, action) => {
-    state.push({
-      id: ++id,
-      task: action.payload.task,
-      completed: false,
-    });
-  },
-  [removeTask.type]: (state, action) => {
-    return state.filter((task) => task.id !== action.payload.id);
-  },
-  [completeTask.type]: (state, action) => {
-    return state.map((v) => (v.id === action.payload.id ? { ...v, completed: true } : v));
+const taskSlice = createSlice({
+  initialState: [],
+  name: "tasks",
+  reducers: {
+    addTask: (state, action) => {
+      state.push({
+        id: ++id,
+        task: action.payload.task,
+        completed: false,
+      });
+    },
+    removeTask: (state, action) => {
+      return state.filter((task) => task.id !== action.payload.id);
+    },
+    completeTask: (state, action) => {
+      return state.map((v) => (v.id === action.payload.id ? { ...v, completed: true } : v));
+    },
   },
 });
+
+export const {addTask,removeTask,completeTask} = taskSlice.actions
+export default taskSlice.reducer;
